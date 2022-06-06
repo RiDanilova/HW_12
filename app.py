b985390f7125133cad7cb7148949ebad70b0bb0c
RiDanilova <ri.charminglady@gmail.com>
2022-06-06 12:48:26 +0300

    improvements

--- app.py
+++ app.py
@@ -6,26 +6,6 @@ from loader.views import loader_blueprint
 
 app = Flask(__name__)
 
-"""
-@app.route("/")
-def page_index():
-    pass
-
-
-@app.route("/list")
-def page_tag():
-    pass
-
-
-@app.route("/post", methods=["GET", "POST"])
-def page_post_form():
-    pass
-
-
-@app.route("/post", methods=["POST"])
-def page_post_upload():
-    pass
-"""
 app.register_blueprint(main_blueprint)
 app.register_blueprint(loader_blueprint)
 
--- loader/views.py
+++ loader/views.py
@@ -11,6 +11,7 @@ logging.basicConfig(filename="logger.log", level=logging.INFO)
 
 
 @loader_blueprint.route("/post")
+def create_new_post_page():
     return render_template("post_form.html")
 
 
